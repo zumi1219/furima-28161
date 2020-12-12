bb6e1f003999d8596a009ff3112a3116d2623e69
zumi1219 <zumiki12@gmail.com>
2020-12-12 21:34:24 +0900

    商品情報編集機能の実装

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,6 @@
 class ProductsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
+  before_action :set_item, only: [:show, :edit, :update]
 
   def index
     @product = Product.all.order(created_at: "DESC")
@@ -21,6 +22,23 @@ class ProductsController < ApplicationController
     else
       render :new
     end
+  end
+
+  def edit
+    @product = Product.find(params[:id])
+  end
+
+  def update
+    @product = Product.find(params[:id])
+    if @product.update(product_params)
+      redirect_to item_path
+    else
+      render :edit
+  end
+end
+
+  def set_item
+    @product = Product.find(params[:id])
   end
 
   private
