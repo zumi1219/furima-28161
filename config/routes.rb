3d9371371e80d06e9e2f9096644ee249223e660d
zumi1219 <zumiki12@gmail.com>
2020-12-13 12:12:00 +0900

    商品削除機能

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
    # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   devise_for :users
   root to: "products#index"
-  resources :products,only: [:index, :new, :create, :show, :edit, :update]
+  resources :products,only: [:index, :new, :create, :show, :edit, :update, :destroy]
 end
