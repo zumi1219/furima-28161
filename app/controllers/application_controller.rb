60f8b1ddb7fc6c1d2e9a6f97b75ac37a75bc872b
zumi1219 <zumiki12@gmail.com>
2020-12-17 19:41:59 +0900

    Update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,4 +5,5 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :encrypted_password, :family_name, :first_name, :family_name_kana, :first_name_kana, :birthday])
   end
+  
 end
